547463f414af4b58585ea638ef93b399bffd4e7f
usernamedrew <59914219+ssvjugg@users.noreply.github.com>
2025-07-18 13:36:19 +0300

    Initial commit

---
+++ bishop-prototype/src/main/java/ru/usernamedrew/bishopprototype/BishopPrototypeApplication.java
@@ -0,0 +1,13 @@
+package ru.usernamedrew.bishopprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BishopPrototypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BishopPrototypeApplication.class, args);
+    }
+
+}
---
+++ synthetic-human-core-starter/src/test/java/ru/usernamedrew/synthetichumancorestarter/SyntheticHumanCoreStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.usernamedrew.synthetichumancorestarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyntheticHumanCoreStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
